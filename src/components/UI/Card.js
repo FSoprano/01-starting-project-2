dc0f7be610b1839fc485ad95367c452f54a2d7f9
KUESTER <kuester@de.ibm.com>
2021-11-30 17:39:02 +0100

    Lesson 91: Adding a reusable Card component

---
+++ src/components/UI/Card.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import classes from "./Card.module.css";
+// Need to import from Card.module.css in this way because we're using 
+// CSS modules.
+const Card = (props) => {
+    
+    return <div className={`${props.className} ${classes.card}`}>{props.children}</div>;
+    /* We inject two CSS stylings here, on is .card from the CSS modules file
+    Card.module.css; the other is classes.input, which is passed down via props 
+    from the className attribute of the <Card> element in AddUser.js.
+    
+    props.children is built into React. 
+    We need it if we want to wrap return output 
+    in custom components (here: Card). 
+    children is a reserved name. Its value will 
+    always be the content in between the custom 
+    component tags.*/
+}
+export default Card;
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,5 +1,6 @@
 import React from 'react';
-
+import Card from '../UI/Card';
+import classes from './AddUser.module.css';
 const AddUser = props => {
 
     const addUserHandler = (event) => {
@@ -7,13 +8,16 @@ const AddUser = props => {
     };
 
     return (
+        <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
             <label htmlFor="username">User</label>
             <input type="text" id="username"></input>
+
             <label htmlFor="age">Age (years)</label>
             <input id="age" type="number"></input>
             <button type="submit">Add User</button>
         </form>
+        </Card>
     );
 };
 // Build a wrapping Card component for this.
